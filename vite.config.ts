d6dcabd3c34f4a0f0c5ceca706826fd0d786ec17
Viktor <victor.eliseev2@gmail.com>
2026-02-08 04:49:46 +0300

    Перенс index.html в корень для GitHub Pages и подправил конфиг в vite

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,8 @@ export default defineConfig({
       plugins: [],
     },
   },
-  root: path.resolve(import.meta.dirname, "client"),
+  root: path.resolve(import.meta.dirname),
+  publicDir: path.resolve(import.meta.dirname, "client", "public"),
   build: {
     outDir: path.resolve(import.meta.dirname, "dist"),
     emptyOutDir: true,
