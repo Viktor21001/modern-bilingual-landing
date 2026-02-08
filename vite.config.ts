7a4ef24d1cbce20e86aa642527d21bac65bdb08d
Viktor <victor.eliseev2@gmail.com>
2026-02-08 04:53:48 +0300

    Удалил ненужные файлы настроек

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-import { metaImagesPlugin } from "./vite-plugin-meta-images";
 
 export default defineConfig({
   base: "./",
-  plugins: [react(), tailwindcss(), metaImagesPlugin()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
